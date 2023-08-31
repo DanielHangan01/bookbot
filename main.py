fa701eb6273e6e9f586aab635b4fa6d6292867c8
DanielHangan01 <danhangan@hotmail.com>
2023-08-31 16:15:16 +0200

    added count letters function

--- main.py
+++ main.py
@@ -2,6 +2,20 @@ def count_words(text):
     words = text.split()
     return len(words)
 
+def count_letters(text):
+    letter_mapping = {}
+    text = text.lower()
+    for char in text:
+        if char == ' ':
+            pass
+        if not char in letter_mapping:
+            letter_mapping[char] = 1
+        else:
+            letter_mapping[char] += 1
+    return letter_mapping
+
+
 with open("books/frankenstein.txt") as f:
     file_contents = f.read()
     print(count_words(file_contents))
+    print(count_letters(file_contents))
